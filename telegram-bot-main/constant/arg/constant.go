e3b1e18d2577c7cda60667c248b74687768a029e
KenHsu <djpkendjpken@gmail.com>
2023-10-24 08:36:46 +0800

    For wrok

--- telegram-bot-main/constant/arg/constant.go
+++ telegram-bot-main/constant/arg/constant.go
@@ -1,1 +1,61 @@
 package arg
+
+type Enum int
+
+const (
+	HELLO Enum = iota
+	HELP
+	HTTP
+)
+
+var list = []string{
+	"hello",
+	"help",
+	"http",
+}
+
+type value struct {
+	introduce string
+	header    string
+	middle    string
+	footer    string
+}
+
+var listMap = map[Enum]value{
+	HELLO: {
+		introduce: "Hello",
+		header:    "Hello",
+		middle:    "Hello",
+		footer:    "Hello",
+	},
+	HELP: {
+		introduce: "Help",
+		header:    "Help",
+		middle:    "Help",
+		footer:    "Help",
+	},
+	HTTP: {
+		introduce: "Http",
+		header:    "HTTP",
+		middle:    "HTTP",
+		footer:    "HTTP",
+	},
+}
+
+func NewEnum(str string) Enum {
+	for i, v := range list {
+		if v == str {
+			return Enum(i)
+		}
+	}
+	return Enum(-1) // Return an invalid enum value to indicate not found.
+}
+func (e Enum) String() string {
+	if e < 0 || int(e) >= len(list) {
+		return "UNKNOWN"
+	}
+	return list[e]
+}
+func (e Enum) getValue() value {
+	return listMap[e]
+}
